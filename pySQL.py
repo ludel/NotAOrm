ec87af5e125f554192155f65c8e71427a7cc9817
ludel <ludovic.delsol@ynov.com>
2018-04-17 00:59:48 +0200

    maj pysql

--- pySQL.py
+++ pySQL.py
@@ -1,3 +1,6 @@
-from model import Requests
+from model import Requests, User, Site
+from Class.condition import Condition, Operator as Op
 
 print(Requests.query.all())
+print(User.query.get(User.pseudo, User.password))
+print(Site.query.delete(Condition(Site.id, Op.equ, 5), False))
