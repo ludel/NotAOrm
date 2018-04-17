1c85b2000a7fb126497eba41aabb63c458ac1820
ludel <ludovic.delsol@ynov.com>
2018-04-17 16:41:53 +0200

    multi condition

--- pySQL.py
+++ pySQL.py
@@ -4,3 +4,5 @@ from Class.condition import Condition, Operator as Op
 print(Requests.query.all())
 print(User.query.get(User.pseudo, User.password))
 print(Site.query.delete(Condition(Site.id, Op.equ, 5), False))
+print(Requests.query.filter([Condition(Requests.id, Op.sup, 2), Condition(Requests.id, Op.inf, 10)]))
+
