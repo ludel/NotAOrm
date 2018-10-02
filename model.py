9b8af56d0d8bdac8ea4edf1822693207bf308c64
ludel <ludovic.delsol@ynov.com>
2018-10-02 16:34:58 +0200

    Delete model

--- model.py
+++
@@ -1,5 +0,0 @@
-from NotAOrm import Table
-
-User = Table(table_name="user", table_row=('id', 'pseudo', 'password'))
-Requests = Table(table_name="requests", table_row=('id', 'number', 'siteId', 'date'))
-Site = Table(table_name="site", table_row=('id', 'url'))
